4700bdc0a6b5d9d500d7caa83d9fa61300a1124c
JamesWithCode <minseok.jeong.james@gmail.com>
2021-11-07 12:37:01 +0900

    셀레니움 스크롤 조정

--- responsive.py
+++ responsive.py
@@ -6,6 +6,7 @@ from time import sleep
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+import math
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 s=Service(ChromeDriverManager().install())
@@ -13,10 +14,16 @@ browser = webdriver.Chrome(service=s,options=options)
 browser.maximize_window()
 browser.get("https://nomadcoders.co")
 
-sizes=[320,480,960,1366,1936]
-
+sizes=[480,960,1366,1936]
+BROWSER_HEIGHT=1056
 
 for size in sizes:
-    browser.set_window_size(size,1056)
+    browser.set_window_size(size,BROWSER_HEIGHT)
+    browser.execute_script("window.scrollTo(0,0)")
     sleep(3)
-print(3)
+    scroll_size=browser.execute_script("return document.body.scrollHeight")
+    total_sections=math.ceil(scroll_size/BROWSER_HEIGHT)
+    for section in range(total_sections+1):
+        browser.execute_script(f"window.scrollTo(0, {(section+1)*BROWSER_HEIGHT })")
+        browser.save_screenshot(f"screenshots/{size}x{section+1}.png")
+        sleep(2)
